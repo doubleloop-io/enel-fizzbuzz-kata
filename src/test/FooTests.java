d815fe0379b96d059aded2e6cbfb6ac86ce665f4
Andrea Vallotti <andrea.vallotti@gmail.com>
2021-03-28 11:17:19 +0200

    Empty project w/ tests

---
+++ src/test/FooTests.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class FooTests {
+    @Test
+    void test() {
+        assertThat(true).isEqualTo(true);
+    }
+}
