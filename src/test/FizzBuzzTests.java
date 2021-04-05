555fcebaf70c8e3a4fe66e1404527dec4cd168a5
Andrea Vallotti <andrea.vallotti@gmail.com>
2021-04-05 12:50:04 +0200

    clean up

--- src/test/FizzBuzzTests.java
+++ src/test/FizzBuzzTests.java
@@ -2,7 +2,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class FooTests {
+public class FizzBuzzTests {
     @Test
     void test() {
         assertThat(true).isEqualTo(true);
