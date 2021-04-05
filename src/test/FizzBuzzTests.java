88c3427a6ba1c6a9b6dc84e15b374cc375ad8954
Andrea Vallotti <andrea.vallotti@gmail.com>
2021-04-05 12:54:35 +0200

    say Fizz

--- src/test/FizzBuzzTests.java
+++ src/test/FizzBuzzTests.java
@@ -1,5 +1,7 @@
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -10,7 +12,14 @@ public class FizzBuzzTests {
         assertThat(fizzBuzz(number)).isEqualTo(expected);
     }
 
+    @ParameterizedTest
+    @ValueSource(ints = {3, 3 * 2})
+    void sayFizz(int number) {
+        assertThat(fizzBuzz(number)).isEqualTo("Fizz");
+    }
+
     private String fizzBuzz(int number) {
+        if (number % 3 == 0) return "Fizz";
         return Integer.toString(number);
     }
 }
