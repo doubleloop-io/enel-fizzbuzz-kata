794f0181a3f985cea83406e582e4ea056a9a1de0
Andrea Vallotti <andrea.vallotti@gmail.com>
2021-04-26 16:43:47 +0200

    create local variable result

--- src/main/FizzBuzzMachine.java
+++ src/main/FizzBuzzMachine.java
@@ -2,6 +2,7 @@ import java.util.stream.IntStream;
 
 public class FizzBuzzMachine {
     public String say(int number) {
+        String result = "";
         if (number % 3 == 0 && number % 5 == 0) return "Fizz" + "Buzz";
         if (number % 3 == 0) return "Fizz";
         if (number % 5 == 0) return "Buzz";
