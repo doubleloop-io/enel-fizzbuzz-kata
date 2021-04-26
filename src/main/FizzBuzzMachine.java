1e789eb45ac69911065559e3cf56eb62316b6b46
Andrea Vallotti <andrea.vallotti@gmail.com>
2021-04-26 16:42:53 +0200

    split FizzBuzz

--- src/main/FizzBuzzMachine.java
+++ src/main/FizzBuzzMachine.java
@@ -2,7 +2,7 @@ import java.util.stream.IntStream;
 
 public class FizzBuzzMachine {
     public String say(int number) {
-        if (number % 3 == 0 && number % 5 == 0) return "FizzBuzz";
+        if (number % 3 == 0 && number % 5 == 0) return "Fizz" + "Buzz";
         if (number % 3 == 0) return "Fizz";
         if (number % 5 == 0) return "Buzz";
         return Integer.toString(number);
