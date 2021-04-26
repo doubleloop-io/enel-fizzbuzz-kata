4df23fbd416f4794e463a80ed5d0e3d102311563
Andrea Vallotti <andrea.vallotti@gmail.com>
2021-04-26 16:44:48 +0200

    concatenate Fizz

--- src/main/FizzBuzzMachine.java
+++ src/main/FizzBuzzMachine.java
@@ -4,7 +4,10 @@ public class FizzBuzzMachine {
     public String say(int number) {
         String result = "";
         if (number % 3 == 0 && number % 5 == 0) return "Fizz" + "Buzz";
-        if (number % 3 == 0) return "Fizz";
+        if (number % 3 == 0) {
+            result += "Fizz";
+            return "Fizz";
+        }
         if (number % 5 == 0) return "Buzz";
         return Integer.toString(number);
     }
