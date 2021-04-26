182977ddeb848f32d2e297f950c5ae7845d9d755
Andrea Vallotti <andrea.vallotti@gmail.com>
2021-04-26 17:44:53 +0200

    implement 7 -> Bang

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,7 +2,8 @@ public class Main {
     public static void main(String[] args) {
         FizzBuzzMachine fizzBuzz = new FizzBuzzMachine(new DivisorAndWord[]{
                 new DivisorAndWord(3, "Fizz"),
-                new DivisorAndWord(5, "Buzz")
+                new DivisorAndWord(5, "Buzz"),
+                new DivisorAndWord(7, "Bang")
         });
 
         for (String fizzBuzzed : fizzBuzz.say(1, 100)) {
