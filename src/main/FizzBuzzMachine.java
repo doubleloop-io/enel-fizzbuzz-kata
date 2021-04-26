ff016eadccd34b2fa686a7d2c7b4ce3d20b52c96
Andrea Vallotti <andrea.vallotti@gmail.com>
2021-04-26 16:49:25 +0200

    actually use result

--- src/main/FizzBuzzMachine.java
+++ src/main/FizzBuzzMachine.java
@@ -6,11 +6,9 @@ public class FizzBuzzMachine {
         if (number % 3 == 0 && number % 5 == 0) return "Fizz" + "Buzz";
         if (number % 3 == 0) {
             result += "Fizz";
-            return "Fizz";
         }
         if (number % 5 == 0) {
             result += "Buzz";
-            return "Buzz";
         }
         if (!result.isEmpty()) return result;
         return Integer.toString(number);
