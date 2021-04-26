31470835993430d3dd590bbec53af32890d3f082
Andrea Vallotti <andrea.vallotti@gmail.com>
2021-04-26 17:07:39 +0200

    extract isDivisibleBy

--- src/main/FizzBuzzMachine.java
+++ src/main/FizzBuzzMachine.java
@@ -3,10 +3,14 @@ import java.util.stream.IntStream;
 public class FizzBuzzMachine {
     public String say(int number) {
         String result = "";
-        if (number % 3 == 0) result += "Fizz";
-        if (number % 5 == 0) result += "Buzz";
+        if (isDivisibleBy(number, 3)) result += "Fizz";
+        if (isDivisibleBy(number, 5)) result += "Buzz";
         if (!result.isEmpty()) return result;
         return Integer.toString(number);
+    }
+
+    private boolean isDivisibleBy(int number, int divisor) {
+        return number % divisor == 0;
     }
 
     public String[] say(int from, int count) {
