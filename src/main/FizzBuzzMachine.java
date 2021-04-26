c1023ac62edc59d2ee39c3e5a672c395c924894d
Andrea Vallotti <andrea.vallotti@gmail.com>
2021-04-26 17:15:47 +0200

    remove local variables

--- src/main/FizzBuzzMachine.java
+++ src/main/FizzBuzzMachine.java
@@ -3,12 +3,8 @@ import java.util.stream.IntStream;
 public class FizzBuzzMachine {
     public String say(int number) {
         String result = "";
-        int divisor1 = 3;
-        String word1 = "Fizz";
-        DivisorAndWord divisorAndWord1 = new DivisorAndWord(divisor1, word1);
-        int divisor2 = 5;
-        String word2 = "Buzz";
-        DivisorAndWord divisorAndWord2 = new DivisorAndWord(divisor2, word2);
+        DivisorAndWord divisorAndWord1 = new DivisorAndWord(3, "Fizz");
+        DivisorAndWord divisorAndWord2 = new DivisorAndWord(5, "Buzz");
         if (isDivisibleBy(number, divisorAndWord1.divisor)) result += divisorAndWord1.word;
         if (isDivisibleBy(number, divisorAndWord2.divisor)) result += divisorAndWord2.word;
         if (!result.isEmpty()) return result;
